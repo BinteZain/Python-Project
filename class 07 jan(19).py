e38adea04261ddffc93349a368f7bf7273658f66
Bint e Zain <bintezain4449@gmail.com>
2025-04-11 02:11:36 +0500

    Add files via upload

---
+++ class 07 jan(19).py
@@ -0,0 +1,36 @@
+# Variable Dictionary :
+
+my_dict = {
+    "name"     : "Python" ,
+    "fees"     : 10000 ,
+    "duration" : "3 months"
+}
+
+print(my_dict)
+print(my_dict["fees"])
+
+# for loop in dictionary :
+
+for a in my_dict :
+    print(a)
+    print(my_dict[a])
+
+# return in dictionary : 
+
+a = 1
+b = 4
+def add() :
+    return a+b
+sum = add()
+print(sum)
+
+# dynamic function with parameter and argument in dictionary :
+
+def greet(class_name , quarter_no) :
+    print("Assalam u Alaikum" , class_name , "Welcome to quarter" ,  quarter_no , "Dear Bint e Zain your timing is 7 to 10 pm in every sunday ")
+
+greet("AI Batch" , 1)
+
+print("part 1 complete")
+
+# << practiced by Bint e Zain>>
