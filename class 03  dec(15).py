1d0ed655297b0bffb781c321e2727aa98b3cb7a5
Bint e Zain <bintezain4449@gmail.com>
2025-04-10 11:59:15 +0500

    Add files via upload

---
+++ class 03  dec(15).py
@@ -0,0 +1,48 @@
+# Arithmetic Operators:
+
+a=50
+b=10
+
+print(a+b)        #addition
+print(a-b)        #subtraction
+print(a*b)        #multiplication
+print(a/b,3/2)    #division
+print(a%b)        #modulus/remainder
+print(a**2)       #exponents/square
+print(a//b,3//2)  #floor division
+print("part 1 complete")
+
+# Assignment Operators:
+
+x=10
+print(x)
+
+x=x+5
+print(x)
+
+x+=5
+print(x)
+
+x=x-5
+print(x)
+
+x-=5
+print(x)
+
+print("part 2 complete")
+
+#Comparison (Relational) Operators:
+
+x=10
+y=5
+
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
+
+print("part 3 complete")
+
+# << practiced by Bint e Zain >>
