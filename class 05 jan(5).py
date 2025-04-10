632fb2d00413e7bdc9af3966b9ad2c2e3cc6cf61
Bint e Zain <bintezain4449@gmail.com>
2025-04-10 13:24:48 +0500

    Add files via upload

---
+++ class 05 jan(5).py
@@ -0,0 +1,54 @@
+
+# Variable List :
+
+names = ["Sir" , "Madam" , "Principal" , "Peon" , "Dustman"]
+
+print(names)
+print(names[3] ,"," , names[4])
+
+students_roll_no = ["sad 1" , "maaz 2" , "asad 3 " , "ali 4" , "anus 5 " , "faiz 6" , "zaid 7" , "zain 8 "]
+print(students_roll_no)
+print("Topper students"  , students_roll_no[1:5])
+
+print("part 1 complete")
+
+# For Loop :
+
+for num in range(1,6) :
+      print(num)                              # (starting point with condition)
+
+print("Table of 2")
+
+for num in range(1,11):
+      print("2*" , num , "=" , 2*num)         #(with increment)
+
+print("part 2 complete")      
+
+# If  /  Else :
+
+a = 10
+
+if a % 2 == 0 : 
+      print("Even Number")        
+else :      
+      print("Odd Number")               # (if statement is print)
+
+# If  /  Elif  /  Else :
+
+total_marks = int(input("Enter the value:-"))
+
+if total_marks == 700 :
+      print("Weldone Boy")
+
+elif total_marks >= 500 :
+      print("Keep it up")
+
+elif total_marks >= 300 :
+      print("You have to better your effort")
+
+else :
+      print("You are failed")           # (statement print according to yourmarks)        
+
+print("part 3 complete")
+
+# << practiced by Bint e Zain >>
