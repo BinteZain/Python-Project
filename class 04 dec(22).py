f0985c405b65eee1f97d37175be5c095344bf9e8
Bint e Zain <bintezain4449@gmail.com>
2025-04-10 12:39:29 +0500

    Add files via upload

---
+++ class 04 dec(22).py
@@ -0,0 +1,20 @@
+# Logical Operators:
+
+x=10
+y=5
+print("and")
+print(x>y and x<20)                 # (true)
+print(x>y and x<20 and x==y)        # (false)
+
+print("or")
+print(x>y or x<20)                  # (true)
+print(x>y or x<y)                   # (true)
+print(x<y or x==y)                  # (false)
+
+print("not")
+print(not (x==y) )                  # (true)
+print(not (x!=y) )                  # (false)
+
+print("part 1 complete")
+
+#  << practiced by Bint e Zain >>
