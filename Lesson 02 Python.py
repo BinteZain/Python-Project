1c309e5189e78f02e22fd9ca4fbd2d54c308249c
Bint e Zain <bintezain4449@gmail.com>
2025-04-12 14:05:15 +0500

    Add files via upload

---
+++ Lesson 02 Python.py
@@ -0,0 +1,29 @@
+# Python Data Types :
+
+num_int : int = 10                                   # (Integer)
+
+print(type(num_int) , "num_int = " , num_int)
+
+num_float : float = 3.10                          # (Floating-Point)
+
+print(type(num_float) , "num_float = " , num_float)
+
+num_complex : complex = 2 + 3j                   # (Complex)
+
+print(type(num_complex) , "num_complex = " , num_complex)
+
+z = 5 + 7j                                      # (Accessing real and imaginary parts) (agar j nahi likhenge to int value bhi show nahi hogi)
+
+print("Real Part = " , z.real)
+
+print("Imaginary Part = " , z.imag)
+
+frozen_set = [1 , 2 , 1 , 4 , 5 , 2]           # (Frozen Set)
+
+print(type(frozen_set) , "frozen_set" , frozen_set)
+
+byte_data : bytes = b"hello"                   # (Bytes) (singal quotation mein a jayega)
+
+print(type(byte_data) , "byte_data = " , byte_data)
+
+# << practiced by Bint e Zain >>
